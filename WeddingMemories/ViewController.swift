293fc22734cb4f6ce650aa0b4ee1e35fd069262f
Tyler Lafferty <tylerlafferty4@gmail.com>
2017-01-24 12:57:36 -0500

    Initial Commit

---
+++ WeddingMemories/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeddingMemories
+//
+//  Created by Tyler Lafferty on 1/24/17.
+//  Copyright © 2017 Tyler Lafferty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
